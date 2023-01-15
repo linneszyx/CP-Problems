b603abe33ebde1b899410d1dd84683418dbc03ac
linneszyx <itssuryanshcooldude346@gmail.com>
2023-01-16 00:13:29 +0530

    hackerrank

---
+++ Digit_Frequency.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    char s;
+    int i;
+    int arr[] = {0,0,0,0,0,0,0,0,0,0};
+    while (scanf("%c",&s)==1)
+    {
+        if(s>='0' && s<='9')
+        {
+            arr[s-'0']+=1;
+        }
+    }
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    
+    
+    
+    return 0;
+}
