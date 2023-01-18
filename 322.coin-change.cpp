a28e7e01aa618748d64e1f37242ce423343f8afd
linneszyx <itssuryanshcooldude346@gmail.com>
2023-01-19 00:20:04 +0530

    leetcode

---
+++ 322.coin-change.cpp
@@ -0,0 +1,78 @@
+/*
+ * @lc app=leetcode id=322 lang=cpp
+ *
+ * [322] Coin Change
+ *
+ * https://leetcode.com/problems/coin-change/description/
+ *
+ * algorithms
+ * Medium (41.74%)
+ * Likes:    15048
+ * Dislikes: 350
+ * Total Accepted:    1.3M
+ * Total Submissions: 3.1M
+ * Testcase Example:  '[1,2,5]\n11'
+ *
+ * You are given an integer array coins representing coins of different
+ * denominations and an integer amount representing a total amount of money.
+ * 
+ * Return the fewest number of coins that you need to make up that amount. If
+ * that amount of money cannot be made up by any combination of the coins,
+ * return -1.
+ * 
+ * You may assume that you have an infinite number of each kind of coin.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: coins = [1,2,5], amount = 11
+ * Output: 3
+ * Explanation: 11 = 5 + 5 + 1
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: coins = [2], amount = 3
+ * Output: -1
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: coins = [1], amount = 0
+ * Output: 0
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= coins.length <= 12
+ * 1 <= coins[i] <= 2^31 - 1
+ * 0 <= amount <= 10^4
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount+1,0);
+        dp[0]  = 0;
+        for (int i = 1; i <= amount; i++)
+        {
+            dp[i] = INT_MAX;
+            for(int c : coins){
+                if(i-c>=0 and dp[i-c]!=INT_MAX){
+                    dp[i] = min(dp[i],dp[i-c]+1);
+                }
+            }
+        }
+    return dp[amount]==INT_MAX ? -1 : dp[amount];
+    }
+};
+// @lc code=end
+
