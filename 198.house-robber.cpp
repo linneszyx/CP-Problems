fc2563fac847ff0705b1425f61741fac3e61e932
linnesyzx <itssuryanshcooldude346@gmail.com>
2023-02-02 04:17:26 +0530

    leetcode

---
+++ 198.house-robber.cpp
@@ -0,0 +1,73 @@
+/*
+ * @lc app=leetcode id=198 lang=cpp
+ *
+ * [198] House Robber
+ *
+ * https://leetcode.com/problems/house-robber/description/
+ *
+ * algorithms
+ * Medium (49.21%)
+ * Likes:    16712
+ * Dislikes: 326
+ * Total Accepted:    1.5M
+ * Total Submissions: 3.1M
+ * Testcase Example:  '[1,2,3,1]'
+ *
+ * You are a professional robber planning to rob houses along a street. Each
+ * house has a certain amount of money stashed, the only constraint stopping
+ * you from robbing each of them is that adjacent houses have security systems
+ * connected and it will automatically contact the police if two adjacent
+ * houses were broken into on the same night.
+ * 
+ * Given an integer array nums representing the amount of money of each house,
+ * return the maximum amount of money you can rob tonight without alerting the
+ * police.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: nums = [1,2,3,1]
+ * Output: 4
+ * Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+ * Total amount you can rob = 1 + 3 = 4.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: nums = [2,7,9,3,1]
+ * Output: 12
+ * Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house
+ * 5 (money = 1).
+ * Total amount you can rob = 2 + 9 + 1 = 12.
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= nums.length <= 100
+ * 0 <= nums[i] <= 400
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int rob(vector<int>& nums) {    
+        int n = nums.size();
+        int p2 = nums[0];
+        if(n==1) return nums[0];
+        int p1 = max(p2,nums[1]);
+        for(int i=2;i<n;i++){
+            int cur_house = max(p1,p2+nums[i]);
+            p2 = p1;
+            p1 = cur_house;
+        }
+        return p1;
+    }
+};
+// @lc code=end
+
