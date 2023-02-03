2a7696acef356df7235c60220cd24dcb5e8c714b
linnesyzx <itssuryanshcooldude346@gmail.com>
2023-02-03 18:37:56 +0530

    starters74

--- Facebook.cpp
+++ Facebook.cpp
@@ -4,7 +4,7 @@ The King Of The Pirates : next level
 */
 #include <bits/stdc++.h>
 using namespace std;
-#define gc getchar_unlocked
+
 #define fo(i, n) for (int i = 0; i < n; i++)
 #define Fo(i, k, n) for (int i = k; k < n ? i < n : i > n; k < n ? i += 1 : i -= 1)
 #define ll long long
