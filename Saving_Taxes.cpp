09df8a28b51c97549072ad437a7359a271ca03e2
linnesyzx <itssuryanshcooldude346@gmail.com>
2023-02-03 18:35:17 +0530

    starters75

--- Saving_Taxes.cpp
+++ Saving_Taxes.cpp
@@ -4,7 +4,6 @@ The King Of The Pirates : next level
 */
 #include <bits/stdc++.h>
 using namespace std;
-#define gc getchar_unlocked
 #define fo(i, n) for (i = 0; i < n; i++)
 #define Fo(i, k, n) for (i = k; k < n ? i < n : i > n; k < n ? i += 1 : i -= 1)
 #define ll long long
