a8a44759c76ce0a250ef43b0049ac01b9c33f19c
linneszyx <itssuryanshcooldude346@gmail.com>
2023-02-04 20:41:55 +0530

    starter76

---
+++ Zero_String.cpp
@@ -0,0 +1,71 @@
+/*
+Author : linneszyx
+The King Of The Pirates : next level
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define gc getchar_unlocked
+#define fo(i, n) for (i = 0; i < n; i++)
+#define Fo(i, k, n) for (i = k; k < n ? i < n : i > n; k < n ? i += 1 : i -= 1)
+#define ll long long
+#define deb(x) cout << #x << "=" << x << endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, false, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+#define mod 1000000007
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+/**
+ * If the number of 1's is less than the number of 0's, then the answer is the number of 1's.
+ * Otherwise, the answer is the number of 0's plus 1
+ */
+void waifuuuuu()
+{
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    int c1 = 0, c2 = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (s[i] == '1')
+            c1++;
+        else
+            c2++;
+    }
+    if (c2 == n)
+        cout << 0 << endl;
+    else if (c1 == n)
+        cout << 1 << endl;
+    else if (c1 == c2)
+        cout << c1 << endl;
+    else if (c1 < c2)
+        cout << c1 << endl;
+    else
+        cout << c2 + 1 << endl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        waifuuuuu();
+    }
+    return 0;
+}
