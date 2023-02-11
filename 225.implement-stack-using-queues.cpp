9abe3b39b034b9275f1b0dd3a0c22fbeafbcb25b
linnesyzx <itssuryanshcooldude346@gmail.com>
2023-02-12 01:38:38 +0530

    leetcode

---
+++ 225.implement-stack-using-queues.cpp
@@ -0,0 +1,122 @@
+/*
+ * @lc app=leetcode id=225 lang=cpp
+ *
+ * [225] Implement Stack using Queues
+ *
+ * https://leetcode.com/problems/implement-stack-using-queues/description/
+ *
+ * algorithms
+ * Easy (58.08%)
+ * Likes:    3991
+ * Dislikes: 964
+ * Total Accepted:    424.8K
+ * Total Submissions: 728.9K
+ * Testcase Example:  '["MyStack","push","push","top","pop","empty"]\n[[],[1],[2],[],[],[]]'
+ *
+ * Implement a last-in-first-out (LIFO) stack using only two queues. The
+ * implemented stack should support all the functions of a normal stack (push,
+ * top, pop, and empty).
+ *
+ * Implement the MyStack class:
+ *
+ *
+ * void push(int x) Pushes element x to the top of the stack.
+ * int pop() Removes the element on the top of the stack and returns it.
+ * int top() Returns the element on the top of the stack.
+ * boolean empty() Returns true if the stack is empty, false otherwise.
+ *
+ *
+ * Notes:
+ *
+ *
+ * You must use only standard operations of a queue, which means that only push
+ * to back, peek/pop from front, size and is empty operations are valid.
+ * Depending on your language, the queue may not be supported natively. You may
+ * simulate a queue using a list or deque (double-ended queue) as long as you
+ * use only a queue's standard operations.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input
+ * ["MyStack", "push", "push", "top", "pop", "empty"]
+ * [[], [1], [2], [], [], []]
+ * Output
+ * [null, null, null, 2, 2, false]
+ *
+ * Explanation
+ * MyStack myStack = new MyStack();
+ * myStack.push(1);
+ * myStack.push(2);
+ * myStack.top(); // return 2
+ * myStack.pop(); // return 2
+ * myStack.empty(); // return False
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * 1 <= x <= 9
+ * At most 100 calls will be made to push, pop, top, and empty.
+ * All the calls to pop and top are valid.
+ *
+ *
+ *
+ * Follow-up: Can you implement the stack using only one queue?
+ *
+ */
+
+// @lc code=start
+class MyStack
+{
+    queue<int> q1, q2;
+public:
+    MyStack()
+    {
+        
+    }
+
+    void push(int x)
+    {
+       q1.push(x);
+    }
+
+    int pop()
+    {   
+        if(q1.empty()) return -1;
+        while(q1.size()!=1){
+            q2.push(q1.front());
+            q1.pop();
+        }
+        int top = q1.front();
+        q1.pop();
+        while(q2.size()){
+            q1.push(q2.front());
+            q2.pop();
+        }
+        return top;
+    }
+    int top()
+    {
+        if(q1.empty()) return -1;
+        return q1.back();
+    }
+
+    bool empty()
+    {
+        return q1.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
+// @lc code=end
