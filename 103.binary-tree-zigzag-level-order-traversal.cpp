180dff38df73ee4f3d4376dcc7cc2a8e333e4950
linnesyzx <itssuryanshcooldude346@gmail.com>
2023-02-19 19:07:13 +0530

    leetcode

---
+++ 103.binary-tree-zigzag-level-order-traversal.cpp
@@ -0,0 +1,90 @@
+/*
+ * @lc app=leetcode id=103 lang=cpp
+ *
+ * [103] Binary Tree Zigzag Level Order Traversal
+ *
+ * https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/description/
+ *
+ * algorithms
+ * Medium (55.42%)
+ * Likes:    8595
+ * Dislikes: 227
+ * Total Accepted:    902.4K
+ * Total Submissions: 1.6M
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * Given the root of a binary tree, return the zigzag level order traversal of
+ * its nodes' values. (i.e., from left to right, then right to left for the
+ * next level and alternate between).
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: root = [3,9,20,null,null,15,7]
+ * Output: [[3],[20,9],[15,7]]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: root = [1]
+ * Output: [[1]]
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: root = []
+ * Output: []
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The number of nodes in the tree is in the range [0, 2000].
+ * -100 <= Node.val <= 100
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> res;
+        if (!root) return res;
+        queue<TreeNode*> q;
+        q.push(root);
+        bool leftToRight = true;
+        while (!q.empty()) {
+            int size = q.size();
+            vector<int> level(size);
+            for (int i = 0; i < size; i++) {
+                TreeNode* node = q.front();
+                q.pop();
+                int index = leftToRight ? i : (size - 1 - i);
+                level[index] = node->val;
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
+            res.push_back(level);
+            leftToRight = !leftToRight;
+        }
+        return res;
+    }
+};
+// @lc code=end
+
