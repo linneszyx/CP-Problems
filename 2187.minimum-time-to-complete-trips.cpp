0953d0006b2d17e5e27ea9cd5ecec4fa17d44318
linnesyzx <itssuryanshcooldude346@gmail.com>
2023-03-07 19:28:59 +0530

    leetcode

---
+++ 2187.minimum-time-to-complete-trips.cpp
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=2187 lang=cpp
+ *
+ * [2187] Minimum Time to Complete Trips
+ */
+
+// @lc code=start
+#define ll long long
+class Solution
+{
+public:
+    long long minimumTime(vector<int> &time, int totalTrips)
+    {
+        ll start = 1;
+        ll end = 1e14;
+        while (start <= end)
+        {
+            ll trip = 0;
+            ll mid = start + (end - start) / 2;
+            for (int i = 0; i < time.size(); i++)
+            {
+                trip += mid / time[i];
+            }
+            if (trip < totalTrips)
+            {
+                start = mid + 1;
+            }
+            else
+            {
+                end = mid - 1;
+            }
+        }
+        return start;
+    }
+};
+// @lc code=end
