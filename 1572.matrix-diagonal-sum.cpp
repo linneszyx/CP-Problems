8f95364f2a9c5d9bbc064584da5f24b9aa4a0ca1
linneszyx <itssuryanshcooldude346@gmail.com>
2023-05-08 18:41:36 +0530

    leetcode

---
+++ 1572.matrix-diagonal-sum.cpp
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=1572 lang=cpp
+ *
+ * [1572] Matrix Diagonal Sum
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += mat[i][i];
+            sum += mat[i][n - i - 1];
+        }
+        if (n % 2 == 1) {
+            sum -= mat[n / 2][n / 2];
+        }
+        return sum;
+    }
+};
+// @lc code=end
+
