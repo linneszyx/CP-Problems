b0cc10cc9bec96b959cdbdee9d5e49a56e3226ff
linneszyx <itssuryanshcooldude346@gmail.com>
2023-05-13 20:31:03 +0530

    leetcode

---
+++ 2466.count-ways-to-build-good-strings.cpp
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=2466 lang=cpp
+ *
+ * [2466] Count Ways To Build Good Strings
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> dp;
+    const int mod = 1e9+7;
+    int solve(int low,int high,int zero,int one,int str){
+        if(str>high) return 0;
+        if(dp[str]!=-1) return dp[str];
+        return dp[str] = ((str>=low) +solve(low,high,zero,one,str+zero)%mod+solve(low,high,zero,one,str+one)%mod)%mod;
+    }
+    int countGoodStrings(int low, int high, int zero, int one) {
+        dp.resize(100002,-1);
+        return solve(low,high,zero,one,0);
+    }
+};
+// @lc code=end
+
