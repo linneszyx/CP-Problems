f8c096701bea65d94f31dd14cc980dfb8d652da8
linneszyx <itssuryanshcooldude346@gmail.com>
2023-05-29 14:00:27 +0530

    codechef

--- Good_Turn.cpp
+++ Good_Turn.cpp
@@ -1,9 +1,5 @@
 #include<bits/stdc++.h>
 using namespace std;
-/**
- * It takes two integers as input and prints YES if the sum of the two integers is greater than 6 and
- * NO otherwise.
- */
 void solve(){
     int x,y;
     cin>>x>>y;
