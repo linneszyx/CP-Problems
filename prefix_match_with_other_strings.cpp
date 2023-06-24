6524bdd1565b294322bd4abdea95e0498cc94924
linneszyx <itssuryanshcooldude346@gmail.com>
2023-06-24 19:14:24 +0530

    potd day 26

---
+++ prefix_match_with_other_strings.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{   
+public:
+    int klengthpref(string arr[], int n, int k, string str){    
+        // code here 
+       int count = 0;
+       for(int i=0;i<n;i++){
+        bool flag = true;
+        for(int j=0;j<k;j++){
+            if(arr[i][j] != str[j]){
+                flag = false;
+                break;
+            }
+        }
+        if(flag){
+            count++;
+        }
+       }
+         return count;
+
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        string arr[n];
+        for(int i=0;i<n;i++)
+        {
+            string s;
+            cin>>arr[i];
+            
+        }
+        int k;
+        cin>>k;
+        string str;
+        cin>>str;
+        
+        Solution ob;
+        cout << ob.klengthpref(arr, n, k, str) << endl;
+    }
+    return 0; 
+} 
+
+// } Driver Code Ends
