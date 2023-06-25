26a34f9e4f9dce2b8347e640d8850a11d906acd5
linneszyx <itssuryanshcooldude346@gmail.com>
2023-06-25 10:58:40 +0530

    potd day 27

---
+++ unique_rows_in_boolean_matrix.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+#define MAX 1000
+
+vector<vector<int>> uniqueRow(int M[MAX][MAX],int row,int col);
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    // #define MAX 1000
+    vector<vector<int>> uniqueRow(int M[MAX][MAX],int row,int col)
+    {
+        //Your code here
+        vector<vector<int>> res;
+        unordered_set<string> s;
+        for(int i=0;i<row;i++){
+            string str="";
+            for(int j=0;j<col;j++){
+                str+=to_string(M[i][j]);
+            }
+            if(s.find(str)==s.end()){
+                s.insert(str);
+                vector<int> v;
+                for(int j=0;j<col;j++){
+                    v.push_back(M[i][j]);
+                }
+                res.push_back(v);
+            }
+        }
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+    	int n,m;
+    	cin>>n>>m;
+    	int a[MAX][MAX];
+    	for(int i=0;i<n;i++)
+    	for(int j=0;j<m;j++)
+    	cin>>a[i][j];
+    	Solution ob;
+    	vector<vector<int>> vec = ob.uniqueRow(a,n,m);
+    	for(int i = 0;i<vec.size();i++){
+    	    for(int x : vec[i]){
+    	        cout<<x<<" ";
+    	    }
+    	    cout<<"$";
+    	}
+    	cout<<endl;
+    }
+}
+
+// } Driver Code Ends
