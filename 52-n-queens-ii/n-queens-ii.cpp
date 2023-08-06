0faf3f845fdfac6f3656594d5e3654048329e295
Suryansh Upadhyay <itssuryanshcooldude346@gmail.com>
2023-08-06 10:15:25 +0530

    Time: 3 ms (85.77%) | Memory: 6 MB (91.94%) - LeetSync

---
+++ 52-n-queens-ii/n-queens-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int solve(vector<bool>&col,vector<bool>&diag,vector<bool>&anti_diag,int row){
+        int n = size(col),count=0;
+        if(row==n) return 1;
+        for(int column = 0; column < n; column++)           
+		if(!col[column] && !diag[row + column] && !anti_diag[row - column + n - 1]){ 
+			col[column] = diag[row + column] = anti_diag[row - column + n - 1] = true;
+			count += solve(col, diag, anti_diag, row + 1); 
+			col[column] = diag[row + column] = anti_diag[row - column + n - 1] = false; 
+		}                                
+	return count;
+    }
+    int totalNQueens(int n) {
+        vector<bool> col(n),diag(2*n-1),anti_diag(2*n-1);
+        return solve(col,diag,anti_diag,0);
+    }
+};
