6d7b34c91927b1931b832092ee6cb36059674949
Suryansh Upadhyay <itssuryanshcooldude346@gmail.com>
2023-08-30 23:32:35 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Missing element of AP/missing-element-of-ap.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+    int findMissing(int arr[], int n) {
+        // code here
+        int x=(arr[n-1]-arr[0])/n;
+        for(int i=0;i<n-1;i++){
+            if(arr[i]+x !=arr[i+1]) return arr[i]+x;
+        }
+        return -1;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.findMissing(arr, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
