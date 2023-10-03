ea77b2590605c1abcb85ef464690cb9e8b71bd53
Suryansh Upadhyay <itssuryanshcooldude346@gmail.com>
2023-10-03 13:21:33 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Column name from a given column number/column-name-from-a-given-column-number.java
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+import java.lang.*; 
+class GfG
+{
+    public static void main (String[] args)
+    {
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            long n = sc.nextLong();
+    		System.out.println (new Solution().colName (n));
+        }
+        
+    }
+}
+
+// Contributed By: Pranay Bansal 
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    String colName (long n)
+    {
+        // your code here
+         String ans="";
+        int rem;
+        while(n!=0)
+        {
+            n=n-1;
+            rem=(int)(n%26);
+            ans=(char)(rem+65)+ans;
+            n=n/26;
+        }
+        return ans;
+    }
+}
