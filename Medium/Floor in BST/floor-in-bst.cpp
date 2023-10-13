ac37e0cd47d90cdd6184f3b53c64784770b4b98f
Suryansh Upadhyay <itssuryanshcooldude346@gmail.com>
2023-10-13 20:07:41 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Floor in BST/floor-in-bst.cpp
@@ -0,0 +1,81 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Node {
+    int data;
+    Node *right;
+    Node *left;
+
+    Node(int x) {
+        data = x;
+        right = NULL;
+        left = NULL;
+    }
+};
+
+
+// } Driver Code Ends
+// Function to search a node in BST.
+class Solution{
+
+public:
+     int ans=-1;
+    int floor(Node* root, int x) {
+        if(!root){
+        return ans;
+        }
+        else if(root->data==x){
+            ans=x;
+            return ans;
+        }
+        else if(root->data<x){
+            ans=max(ans,root->data);
+            floor(root->right,x);
+            return ans;
+        }
+        else{
+            floor(root->left,x);
+            return ans;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+Node *insert(Node *tree, int val) {
+    Node *temp = NULL;
+    if (tree == NULL) return new Node(val);
+
+    if (val < tree->data) {
+        tree->left = insert(tree->left, val);
+    } else if (val > tree->data) {
+        tree->right = insert(tree->right, val);
+    }
+
+    return tree;
+}
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        Node *root = NULL;
+
+        int N;
+        cin >> N;
+        for (int i = 0; i < N; i++) {
+            int k;
+            cin >> k;
+            root = insert(root, k);
+        }
+
+        int s;
+        cin >> s;
+        Solution obj;
+        cout << obj.floor(root, s) << "\n";
+    }
+}
+
+// } Driver Code Ends
