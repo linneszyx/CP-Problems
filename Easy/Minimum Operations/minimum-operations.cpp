2b7cf129bfa3ef0e51dfc5f11bfd0db8683e270c
Suryansh Upadhyay <itssuryanshcooldude346@gmail.com>
2023-10-26 05:10:39 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Minimum Operations/minimum-operations.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+  public:
+    int minOperation(int n)
+    {
+        //code here.
+        int ans = 0;
+        while(n>1){
+            if(n%2==0) ans++;
+            else ans+=2;
+            n/=2;
+        }
+        return ans+1;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	    {
+	        int n;
+	        cin>>n;
+	        Solution ob;
+	        cout<<ob.minOperation(n)<<endl;
+	    }
+}
+// } Driver Code Ends
