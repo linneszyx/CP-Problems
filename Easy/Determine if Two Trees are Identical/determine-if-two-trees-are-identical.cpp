f9b5b38f9572fb0cfa1a6830db44d7acba9ddd32
Suryansh Upadhyay <itssuryanshcooldude346@gmail.com>
2023-11-21 01:07:08 +0530

    Updated Solution - GfG to GitHub

--- Easy/Determine if Two Trees are Identical/determine-if-two-trees-are-identical.cpp
+++ Easy/Determine if Two Trees are Identical/determine-if-two-trees-are-identical.cpp
@@ -35,6 +35,24 @@ struct Node
 
 
    
+/* A binary tree node
+
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+*/
+
+
+   
 class Solution
 {
     public:
