1cbcfff48c7373cdcb4626cd9658d5d41bd8a4e9
Suryansh Upadhyay <itssuryanshcooldude346@gmail.com>
2024-02-21 14:39:59 +0530

    Updated Solution - GfG to GitHub

--- Hard/Boolean Parenthesization/boolean-parenthesization.cpp
+++ Hard/Boolean Parenthesization/boolean-parenthesization.cpp
@@ -9,74 +9,38 @@ using namespace std;
 
 class Solution{
 public:
-      int mod= 1003;
-
-    int solve(int i, int j, int isTrue, string &s, vector<vector<vector<int>>> &dp){
-        if(i>j) return 0;
-        if(dp[i][j][isTrue] != -1) return dp[i][j][isTrue]; 
-        
-        if(i==j){
-            if(isTrue){
-                if(s[i]=='T') return 1;
-                else{
-                    return 0;
-                }
+    int countWays(int n, string s){
+        int mod = 1003;
+        vector<vector<int>> dp_true(n, vector<int>(n, 0));
+        vector<vector<int>> dp_false(n, vector<int>(n, 0));
+        for (int i = 0; i < n; i++) {
+            if (s[i] == 'T') {
+                dp_true[i][i] = 1;
+            } else {
+                dp_false[i][i] = 1;
             }
-            else{
-                if(s[i]=='F') return 1;
-                else{
-                    return 0;
+        }
+        for (int l = 3; l <= n; l += 2) {
+            for (int i = 0; i < n - l + 1; i++) {
+                int j = i + l - 1;
+                for (int k = i + 1; k < j; k += 2) {
+                    if (s[k] == '&') {
+                        dp_true[i][j] += dp_true[i][k - 1] * dp_true[k + 1][j];
+                        dp_false[i][j] += dp_true[i][k - 1] * dp_false[k + 1][j] + dp_false[i][k - 1] * dp_true[k + 1][j] + dp_false[i][k - 1] * dp_false[k + 1][j];
+                    } else if (s[k] == '|') {
+                        dp_true[i][j] += dp_true[i][k - 1] * dp_false[k + 1][j] + dp_false[i][k - 1] * dp_true[k + 1][j] + dp_true[i][k - 1] * dp_true[k + 1][j];
+                        dp_false[i][j] += dp_false[i][k - 1] * dp_false[k + 1][j];
+                    } else if (s[k] == '^') {
+                        dp_true[i][j] += dp_true[i][k - 1] * dp_false[k + 1][j] + dp_false[i][k - 1] * dp_true[k + 1][j];
+                        dp_false[i][j] += dp_true[i][k - 1] * dp_true[k + 1][j] + dp_false[i][k - 1] * dp_false[k + 1][j];
+                    }
+    
+                    dp_true[i][j] %= mod;
+                    dp_false[i][j] %= mod;
                 }
             }
         }
-        
-        int ways=0;
-        
-        for(int index=i+1; index<j; index+=2){
-            int lt= solve(i,index-1,1,s,dp);
-            int lf= solve(i,index-1,0,s,dp);
-            int rt= solve(index+1,j,1,s,dp);
-            int rf= solve(index+1,j,0,s,dp);
-            
-            if(s[index]=='&'){
-                if(isTrue){
-                    ways= (ways%mod + (lt % mod * rt % mod) % mod ) % mod; 
-                }
-                else{
-                    ways= (ways%mod + (lf % mod * rt % mod)%mod + (lt % mod * rf % mod)%mod + (lf % mod * rf % mod)%mod ) % mod;
-                }
-            }
-            
-            else if(s[index]=='|'){
-                if(isTrue){
-                    ways= (ways%mod + (lf % mod * rt % mod)%mod + (lt % mod * rf % mod)%mod + (lt % mod * rt % mod)% mod )%mod;
-                }
-                else{
-                    ways=(ways%mod + (lf % mod * rf % mod)% mod) % mod;
-                }
-            }
-            else{
-                if(isTrue){
-                    ways= (ways%mod + (lf % mod * rt % mod)%mod + (lt % mod * rf % mod)% mod) % mod;
-                }
-                else{
-                    ways=(ways%mod + (lt % mod * rt % mod)%mod + (lf % mod * rf % mod)%mod) % mod;
-                }
-                
-            }
-            
-            
-        }
-        dp[i][j][isTrue]= ways % mod;
-        return dp[i][j][isTrue];
-        
-        
-    }
-
-    int countWays(int N, string S){
-        
-        vector<vector<vector<int>>>dp(N+1, vector<vector<int>>(N+1, vector<int>(3,-1)));
-        return solve(0,N-1,1,S,dp);
+        return dp_true[0][n - 1] % mod;
     }
 };
 
@@ -86,13 +50,13 @@ int main(){
     int t;
     cin>>t;
     while(t--){
-        int N;
-        cin>>N;
-        string S;
-        cin>>S;
+        int n;
+        cin>>n;
+        string s;
+        cin>>s;
         
         Solution ob;
-        cout<<ob.countWays(N, S)<<"\n";
+        cout<<ob.countWays(n, s)<<"\n";
     }
     return 0;
 }
