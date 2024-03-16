58eacf09faae4594a13d864ae2a90c7d95360a64
Suryansh Upadhyay <itssuryanshcooldude346@gmail.com>
2024-03-16 11:02:43 +0530

    Updated Solution - GfG to GitHub

--- Easy/Delete without head pointer/delete-without-head-pointer.cpp
+++ Easy/Delete without head pointer/delete-without-head-pointer.cpp
@@ -79,15 +79,15 @@ class Solution
 {
     public:
     //Function to delete a node without any reference to head pointer.
-    void deleteNode(Node *del)
+    void deleteNode(Node *del_node)
     {
-       // Your code here
-       if(del->next==NULL) {
-           del = NULL;
-           return;
+       Node* prev;
+       while(del_node->next!=NULL){
+           swap(del_node->data,del_node->next->data);
+           prev=del_node;
+           del_node=del_node->next;
        }
-       del->data = del->next->data;
-       del->next = del->next->next;
+       prev->next=NULL;
     }
 
 };
