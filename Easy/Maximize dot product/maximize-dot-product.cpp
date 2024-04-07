ef26d6f2c3cc6070d14ad9459087a05769c51fa4
Suryansh Upadhyay <itssuryanshcooldude346@gmail.com>
2024-04-07 18:27:07 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Maximize dot product/maximize-dot-product.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+		
+	public:
+	int dp[1001][1001];
+	int f(int i,int j,int &n,int &m,int a[],int b[]){
+	    if(j==m) return 0;
+	    if(i==n) return -1e7;
+	    if(dp[i][j]!=-1) return dp[i][j];
+	    return dp[i][j] = max((a[i]*b[j]) + f(i+1,j+1,n,m,a,b),f(i+1,j,n,m,a,b));
+	}
+	int maxDotProduct(int n, int m, int a[], int b[]) 
+	{ 
+		// Your code goes here
+		memset(dp,-1,sizeof(dp));
+		return f(0,0,n,m,a,b);
+	} 
+};
+
+
+//{ Driver Code Starts.
+int main() 
+{
+   
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, m;
+        cin >> n >> m;
+
+        int a[n], b[m];
+
+        for(int i = 0; i < n; i++)
+        	cin >> a[i];
+
+        for(int i = 0; i < m; i++)
+        	cin >> b[i];
+
+       
+
+	    Solution ob;
+	    cout << ob.maxDotProduct(n, m, a, b) << "\n";
+	     
+    }
+    return 0;
+}
+
+// } Driver Code Ends
