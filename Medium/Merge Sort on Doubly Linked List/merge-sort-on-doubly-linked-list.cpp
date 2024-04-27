4f39fefcff17ba2443e68579ed9acf614b190030
Suryansh Upadhyay <itssuryanshcooldude346@gmail.com>
2024-04-27 19:22:27 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Merge Sort on Doubly Linked List/merge-sort-on-doubly-linked-list.cpp
@@ -0,0 +1,107 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node *next, *prev;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+        prev = NULL;
+    }
+};
+
+
+// } Driver Code Ends
+
+/* Structure of the node of the list is as
+struct Node
+{
+    int data;
+    struct Node *next, *prev;
+    Node (int x){
+        data = x;
+        next = prev = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    // Function to sort the given doubly linked list using Merge Sort.
+    struct Node *sortDoubly(struct Node *head) {
+        // Your code here
+        vector<int> v;
+        struct Node * temp = head;
+        while(temp!=NULL) {
+            v.push_back(temp->data);
+            temp=temp->next;
+        }
+        sort(v.begin(),v.end());
+        temp = head;
+        for(int i=0;i<v.size();i+=1) {
+            temp->data = v[i];
+            temp=temp->next;
+        }
+        return head;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+void insert(struct Node **head, int data) {
+    struct Node *temp = new Node(data);
+    if (!(*head))
+        (*head) = temp;
+    else {
+        temp->next = *head;
+        (*head)->prev = temp;
+        (*head) = temp;
+    }
+}
+
+void print(struct Node *head) {
+    struct Node *temp = head;
+    while (head) {
+        cout << head->data << ' ';
+        temp = head;
+        head = head->next;
+    }
+    cout << endl;
+    while (temp) {
+        cout << temp->data << ' ';
+        temp = temp->prev;
+    }
+    cout << endl;
+}
+
+// Utility function to swap two integers
+void swap(int *A, int *B) {
+    int temp = *A;
+    *A = *B;
+    *B = temp;
+}
+
+// Driver program
+int main(void) {
+    long test;
+    cin >> test;
+    while (test--) {
+        int n, tmp;
+        struct Node *head = NULL;
+        cin >> n;
+        while (n--) {
+            cin >> tmp;
+            insert(&head, tmp);
+        }
+        Solution ob;
+        head = ob.sortDoubly(head);
+        print(head);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
