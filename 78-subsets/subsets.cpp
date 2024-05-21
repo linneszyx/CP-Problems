bce2ebe8bb870a6dc3747e4b43e14244b7dc5064
Suryansh Upadhyay <itssuryanshcooldude346@gmail.com>
2024-05-21 19:26:10 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (38.74%) - LeetSync

---
+++ 78-subsets/subsets.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+public:
+    void dfs(vector<int> &nums, int x, vector<int> &path, vector<vector<int>> &res)
+    {
+        if (x == nums.size())
+        {
+            res.push_back(path);
+            return;
+        }
+        dfs(nums, x + 1, path, res);
+        path.push_back(nums[x]);
+        dfs(nums, x + 1, path, res);
+        path.pop_back();
+    }
+    vector<vector<int>> subsets(vector<int> &nums)
+    {
+        vector<vector<int>> res;
+        vector<int> path;
+        dfs(nums, 0, path, res);
+        return res;
+    }
+};
