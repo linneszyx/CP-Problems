7bfcd980034bca8b23bd66fd2450383a584fa569
Suryansh Upadhyay <itssuryanshcooldude346@gmail.com>
2024-07-08 09:37:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Search in Rotated Sorted Array/search-in-rotated-sorted-array.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int search(vector<int>& arr, int key) {
+         int i=0;
+        int j=arr.size()-1;
+        while(i<=j){
+            int mid=(i+j)/2;
+            if(arr[mid]==key) return mid;
+            if(arr[mid]>=arr[i]){
+                if(key>=arr[i] && key<arr[mid] ) j=mid-1;
+                else i=mid+1;
+            }
+            else{
+                if(key>arr[mid] && key<=arr[j]) i=mid+1;
+                else j=mid-1;
+            }
+        }
+        return -1;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        cin.ignore();
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int key;
+        cin >> key;
+        Solution ob;
+        cout << ob.search(arr, key) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
