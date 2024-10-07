baa886c12390c8fc04b90b5d06df7c5b24804459
Suryansh Upadhyay <itssuryanshcooldude346@gmail.com>
2024-10-07 21:34:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Postfix to Infix Conversion/postfix-to-infix-conversion.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    string postToInfix(string s) {
+        int i = 0;
+        stack<string> st;
+        while(i < s.size()){
+            if((s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= 'a' && s[i] <= 'z') || (s[i] >= '0' && s[i] <= '9')){
+                st.push(string(1, s[i]));
+            }
+            else{
+                string t1 = st.top();
+                st.pop();
+                string t2 = st.top();
+                st.pop();
+                st.push('(' + t2 + s[i] + t1 +')');
+            }
+            i++;
+        }
+        return st.top();
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t = 1;
+    cin >> t;
+
+    // freopen ("output_gfg.txt", "w", stdout);
+
+    while (t--) {
+        // Input
+        string postfix;
+        cin >> postfix;
+        // char marker; cin >> marker;
+
+        Solution obj;
+        cout << obj.postToInfix(postfix) << endl;
+
+        // cout << "~\n";
+    }
+    fclose(stdout);
+
+    return 0;
+}
+
+// } Driver Code Ends
