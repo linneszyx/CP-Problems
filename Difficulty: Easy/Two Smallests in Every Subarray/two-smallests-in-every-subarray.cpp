1dc717068db3c200ca63fe58fa60605ee97ff6ee
Suryansh Upadhyay <itssuryanshcooldude346@gmail.com>
2024-10-12 14:33:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Two Smallests in Every Subarray/two-smallests-in-every-subarray.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int pairWithMaxSum(vector<int>& arr) {
+        // code here
+        int n = arr.size();
+        int ans = -1;
+        for(int i=0;i<n-1;i+=1){
+            int s=  arr[i]+arr[i+1];
+            ans=max(s,ans);
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int tc;
+    cin >> tc;
+    cin.ignore();
+
+    while (tc--) {
+        string input;
+        getline(cin, input);
+
+        stringstream ss(input);
+        vector<int> a;
+        int number;
+        while (ss >> number) {
+            a.push_back(number);
+        }
+
+        Solution obj;
+        int res = obj.pairWithMaxSum(a);
+        cout << res << endl;
+    }
+}
+// } Driver Code Ends
