40b73c593e6783f57236200526a365ad9eaaf7a4
Suryansh Upadhyay <itssuryanshcooldude346@gmail.com>
2024-11-18 18:10:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Rotate Array/rotate-array.cpp
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+// Initial function template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int f(vector<int>&arr,int s,int e){
+        while(s<e) {
+            swap(arr[s],arr[e]);
+            s++;
+            e--;
+        }
+    }
+    void rotateArr(vector<int>& arr, int d) {
+        int n = arr.size();
+        if(n==0 or d==0 or d==n) return;
+        d%=n;
+        f(arr,0,d-1);
+        f(arr,d,n-1);
+        f(arr,0,n-1);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int test_case;
+    cin >> test_case;
+    cin.ignore();
+    while (test_case--) {
+
+        int d;
+        vector<int> arr, brr, crr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        getline(cin, input);
+        ss.clear();
+        ss.str(input);
+        while (ss >> number) {
+            crr.push_back(number);
+        }
+        d = crr[0];
+        int n = arr.size();
+        Solution ob;
+        // calling rotateArr() function
+        ob.rotateArr(arr, d);
+
+        // printing the elements of the array
+        for (int i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
