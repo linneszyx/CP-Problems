a80fb61b3988188b446fa57eab0c6e1604ac4fc7
Suryansh Upadhyay <itssuryanshcooldude346@gmail.com>
2024-12-14 17:18:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Search in Rotated Sorted Array/search-in-rotated-sorted-array.cpp
+++ Difficulty: Medium/Search in Rotated Sorted Array/search-in-rotated-sorted-array.cpp
@@ -4,24 +4,31 @@ using namespace std;
 
 
 // } Driver Code Ends
+
+#define vi vector<int>
+#define sz(x) ((int)x.size())
+int f(vi &arr,int tar) {
+    int n = sz(arr);
+    int l=0,h=n-1;
+    while(l<=h) {
+        int m=(l+h)/2;
+        if(arr[m]==tar) return m;
+        else if(arr[m]>=arr[l]) {
+            if(arr[m]>=tar and arr[l]<=tar) h=m-1;
+            else l=m+1;
+        }
+        else {
+            if(arr[m]<=tar and arr[h]>=tar) l=m+1;
+            else h=m-1;
+        }
+    }
+    return -1;
+}
 class Solution {
   public:
     int search(vector<int>& arr, int key) {
-         int i=0;
-        int j=arr.size()-1;
-        while(i<=j){
-            int mid=(i+j)/2;
-            if(arr[mid]==key) return mid;
-            if(arr[mid]>=arr[i]){
-                if(key>=arr[i] && key<arr[mid] ) j=mid-1;
-                else i=mid+1;
-            }
-            else{
-                if(key>arr[mid] && key<=arr[j]) i=mid+1;
-                else j=mid-1;
-            }
-        }
-        return -1;
+    int x = f(arr,key);
+    return x;
     }
 };
 
@@ -43,6 +50,7 @@ int main() {
         cin >> key;
         Solution ob;
         cout << ob.search(arr, key) << endl;
+        cout << "~" << endl;
     }
     return 0;
 }
