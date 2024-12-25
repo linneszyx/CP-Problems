1b5e396a3a7c8b50b4ba2fbd9f5badcb6089d795
Suryansh Upadhyay <itssuryanshcooldude346@gmail.com>
2024-12-25 11:11:48 +0530

    Time: 0 ms (100.00%) | Memory: 22.6 MB (36.36%) - LeetSync

---
+++ 515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.cpp
@@ -0,0 +1,16 @@
+class Solution {
+private:
+  vector<int> ans;
+  void preorder(TreeNode* root, int height) {
+    if (!root) return;
+    if (ans.size() < height) ans.push_back(root->val);
+    else ans[height - 1] = max(ans[height - 1], root->val);
+    preorder(root->left, height + 1);
+    preorder(root->right, height + 1);
+  }
+public:
+  vector<int> largestValues(TreeNode* root) {
+    preorder(root, 1);
+    return ans;
+  }
+};
