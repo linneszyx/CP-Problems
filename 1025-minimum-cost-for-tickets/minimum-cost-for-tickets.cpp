13a782fa7e516ca635c561d51634d9016415bafd
Suryansh Upadhyay <itssuryanshcooldude346@gmail.com>
2024-12-31 10:09:53 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (65.58%) - LeetSync

---
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        int now = 0;
+        int n = days.size();
+        int dp[366] = {0};
+        for (int i = 1; i <= 365; i++) {
+            if (now < n && i == days[now]) {
+                dp[i] = min(dp[i - 1] + costs[0], min(dp[max(0, i - 7)] + costs[1], dp[max(0, i - 30)] + costs[2]));
+                now++;
+            } else {
+                dp[i] = dp[i - 1];
+            }
+        }
+        return dp[days.back()]; 
+    }
+};
