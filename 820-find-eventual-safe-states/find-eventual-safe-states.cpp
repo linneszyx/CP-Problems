5ef6030889a1348c68b8a8bc86a2c41d021aee6b
Suryansh Upadhyay <itssuryanshcooldude346@gmail.com>
2025-01-24 09:08:17 +0530

    Time: 3 ms (95.70%) | Memory: 51.8 MB (88.60%) - LeetSync

---
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool dfs(vector<vector<int>>& graph, int cur, vector<int>& color) {
+        if (color[cur] > 0) return color[cur] == 2;
+        color[cur] = 1;
+        for (int i : graph[cur]) {
+            if (color[i] == 2) continue;
+            if (color[i] == 1 || !dfs(graph, i, color)) return false;
+        }
+        color[cur] = 2;
+        return true;
+    }
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        vector<int> res;
+        vector<int> color(graph.size(), 0);
+        for (int i = 0; i < graph.size(); ++i) {
+            if (dfs(graph, i, color)) res.push_back(i);
+        }
+        return res;
+    }
+};
