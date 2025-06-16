c34c0b92b9b39fcb796f18d927d2919efac2f666
Suryansh Upadhyay <itssuryanshcooldude346@gmail.com>
2025-06-16 13:34:44 +0530

    Time: 1 ms (52.28%) | Memory: 17 MB (61.99%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
@@ -1,26 +1,31 @@
 class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
-     stack<int> stk;
-     for(string &s : tokens) {
-        if(s.size()>1 or isdigit(s[0]))
-        stk.push(stoi(s));
-       else 
-       { int n2 = stk.top();
-        stk.pop();
-        int n1 = stk.top();
-        stk.pop();
-        switch(s[0]) {
-            case '+' : stk.push(n1+n2);
-            break;
-            case '-' : stk.push(n1-n2);
-            break;
-            case '*' : stk.push(n1*n2);
-            break;
-            case '/' : stk.push(n1/n2);
-            break;
-        }}
-    }
+        stack<int> stk;
+        for (string& s : tokens) {
+            if (s.size() > 1 or isdigit(s[0]))
+                stk.push(stoi(s));
+            else {
+                int n2 = stk.top();
+                stk.pop();
+                int n1 = stk.top();
+                stk.pop();
+                switch (s[0]) {
+                case '+':
+                    stk.push(n1 + n2);
+                    break;
+                case '-':
+                    stk.push(n1 - n2);
+                    break;
+                case '*':
+                    stk.push(n1 * n2);
+                    break;
+                case '/':
+                    stk.push(n1 / n2);
+                    break;
+                }
+            }
+        }
         return stk.top();
-     }
+    }
 };
