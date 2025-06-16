19a54cc004fe4dd57b9800a4e86d0ff518b73e70
Suryansh Upadhyay <itssuryanshcooldude346@gmail.com>
2025-06-16 14:01:18 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (35.81%) - LeetSync

--- 32-longest-valid-parentheses/longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/longest-valid-parentheses.cpp
@@ -4,20 +4,20 @@ public:
         int n = s.size();
         if(s=="") return 0;
         stack<int> stk;
-        stk.push(-1);
-        int depth = 0;
+       stk.push(-1);
+        int len = 0;
         for(int i=0;i<n;i+=1)
         {
             if(s[i]=='(') {
                 stk.push(i);
             }
             else {
-                 stk.pop();
+                stk.pop();
                 if(stk.empty())
                 stk.push(i);
-               else depth = max(depth,i-stk.top());
+                else len = max(len,i-stk.top());
             }
         }
-        return depth;
+        return len;
     }
 };
